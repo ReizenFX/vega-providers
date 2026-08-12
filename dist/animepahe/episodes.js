d0a729cb19f0afc6f7623796a8a4fd75ee7b3ebe
Reizen <96108057+ReizenFX@users.noreply.github.com>
2026-08-13 01:01:33 +0530

    Update episodes.js

--- dist/animepahe/episodes.js
+++ dist/animepahe/episodes.js
@@ -1,23 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getEpisodes = void 0;
+
 const getEpisodes = async (args) => {
     const { url, providerContext } = args;
     const { axios } = providerContext;
+    
     try {
         const res = await axios.get(`https://animepahe.pw/api?m=release&id=${url}&sort=episode_asc&page=1`);
         const json = res.data;
-        if (json && json.data && json.data.length > 0) {
+        
+        if (json && json.data) {
             return json.data.map((ep) => ({
                 title: `Episode ${ep.episode}`,
                 link: ep.session,
             }));
         }
-        throw new Error("Bypass");
+        return [];
     } catch (error) {
-        return [
-            { title: "Episode 1", link: "51758da8e3c84e07661ff4a1f517ad189adee18988c96292be89e170fbd49d58" }
-        ];
+        return [];
     }
 };
 exports.getEpisodes = getEpisodes;
